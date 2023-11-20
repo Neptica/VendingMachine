4e2d94cf23b7ed7c30554400d4bde7339e8f43ac
Michael Mumme <neptica@hotmail.com>
2023-11-20 11:29:40 -0600

    Adding windows support for the execution of VendingMachine within the terminal (batch file)

---
+++ run.bat
@@ -0,0 +1,6 @@
+csc /target:library -out:dll/Product.dll Product.cs
+csc /target:library -out:dll/Coin.dll Coin.cs
+csc /r:dll/Coin.dll /target:library -out:dll/CoinBox.dll CoinBox.cs
+csc /r:dll/Coin.dll /r:dll/CoinBox.dll /r:dll/Product.dll /target:library -out:dll/VendingMachine.dll VendingMachine.cs
+csc /r:dll/Coin.dll /r:dll/CoinBox.dll /r:dll/Product.dll /r:dll/VendingMachine.dll -out:dll/Machine.exe VendingMachineMenu.cs
+mono dll/Machine.exe
