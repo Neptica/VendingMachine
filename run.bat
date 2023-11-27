3b08740e43a5a09c464fd11d3a086e8285aa49fc
Hannah Marcus <hmarcus@blc.edu>
2023-11-27 12:06:24 -0600

    changed bat file to work for windows

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ csc /target:library -out:dll/Coin.dll Coin.cs
 csc /r:dll/Coin.dll /target:library -out:dll/CoinBox.dll CoinBox.cs
 csc /r:dll/Coin.dll /r:dll/CoinBox.dll /r:dll/Product.dll /target:library -out:dll/VendingMachine.dll VendingMachine.cs
 csc /r:dll/Coin.dll /r:dll/CoinBox.dll /r:dll/Product.dll /r:dll/VendingMachine.dll -out:dll/Machine.exe VendingMachineMenu.cs
-mono dll/Machine.exe
+dll/Machine.exe
